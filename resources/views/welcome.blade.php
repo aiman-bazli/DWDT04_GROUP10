fdba87422cc0c44c937d7c6171d58432a979e8f8
aiman-bazli <59683532+aiman-bazli@users.noreply.github.com>
2020-11-07 08:23:53 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Sayur</a>
+                    <a href="https://laravel.com/docs">Bayur</a>
                     <a href="https://laracasts.com">Bayam</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
